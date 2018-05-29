e73c04eeb559cd487cb5cb63e95c434be3c565c5
Veronika Machackova <veronikam@Veronikas-MacBook-Pro.local>
2018-05-29 15:03:24 +0200

    queue number shows in console and h1

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 "use strict"
 //fetch https://kea-alt-del.dk/kata-distortion/?count=25
+
+function getData() {
+
+    fetch("https://kea-alt-del.dk/kata-distortion/?count=25").then(res => res.json())
+        .then(showData);
+}
+
+function showData(json) {
+    let queueNumber = json.inQueue;
+    console.log(queueNumber);
+    document.getElementById("queue-count").textContent=queueNumber;
+}
+setInterval(getData, 1000);
