023101cd149d71e8c582f236d24dee73374fa459
Veronika Machackova <veronikam@Veronikas-MacBook-Pro.local>
2018-06-03 19:19:56 +0200

    javascript

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function showData(json) {
 
     childText = document.createElement('h1');
     childText.textContent = queueNumber;
- 
+
     parentDiv.appendChild(childText);
 
     barChart = document.createElement('div');
@@ -31,23 +31,19 @@ function showData(json) {
     parentDiv.insertBefore(barChart, parentDiv.children[0]);
     parentDiv.insertBefore(childText, parentDiv.children[0]);
     // parentDiv.appendChild(barChart);
-kid = parentDiv.children[6];
-kid2 = parentDiv.children[7];
-console.log(kid)
+    kid = parentDiv.children[6];
+    kid2 = parentDiv.children[7];
+    console.log(kid)
     counter++;
+    
     setTimeout(function addWidth() {
         barChart.style.width = (queueNumber * 20 + 'px')
     }, 0);
 
     if (counter > 4) {
-kid.remove();
-kid2.remove();
-
-
+        kid.remove();
+        kid2.remove();
     }
-
 }
-
-
 
 setInterval(getData, 10000);
